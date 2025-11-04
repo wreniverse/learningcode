1e355c258c86560d59129733de020367dad6f455
Warren Pierson <wreniverse@gmail.com>
2025-11-03 20:23:39 -0800

    Initial work

---
+++ src/com/wreniverse/Main.java
@@ -0,0 +1,9 @@
+package com.wreniverse;
+
+public class Main {
+    public static void main(String[] args) {
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(herAge);
+    }
+}
