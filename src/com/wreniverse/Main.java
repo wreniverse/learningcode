66cde087e1952730c53e852b543bd75399dc9168
Warren Pierson <wreniverse@gmail.com>
2026-06-24 15:04:03 -0700

    a reset

--- src/com/wreniverse/Main.java
+++ src/com/wreniverse/Main.java
@@ -1,9 +1,6 @@
 package com.wreniverse;
 
-public class Main {
-    public static void main(String[] args) {
-        int myAge = 30;
-        int herAge = myAge;
-        System.out.println(herAge);
-    }
+class Main {
+
+    
 }
